e6a4d520b016329cf05674e9a130a748167e0ca6
OggyTrueMaster <166068460+OggyTrueMaster@users.noreply.github.com>
2026-08-20 20:50:19 +0200

    Add files via upload

---
+++ OurADCE/OurADCEPass.cpp
@@ -0,0 +1,60 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Operator.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Function.h"
+#include "OurCFG.h"
+
+using namespace llvm;
+
+namespace {
+struct OurADCE : public FunctionPass {
+  std::unordered_map<BasicBlock *, bool> reachableInstructions;
+  bool noLivenessChange;
+
+  static char ID;
+  OurADCE() : FunctionPass(ID) {}
+
+  void findReachableInstructions(Function &F){
+    OurCFG *CFG = new OurCFG(F);
+    CFG->DFS(&F.front());
+
+    for(BasicBlock &BB : F){
+      reachableInstructions[&BB] = false;
+      if(CFG->isReachable(&BB)){
+        reachableInstructions[&BB] = true;
+        noLivenessChange = true;
+      }
+    }
+  }
+
+  void eliminateUnreachableInstructions(Function &F){
+    std::vector<BasicBlock *> BBList;
+    for(BasicBlock &BB : F){
+      BBList.push_back(&BB);
+    }
+
+    for(BasicBlock *BB : BBList){
+      if(!reachableInstructions[BB]){
+        BB->eraseFromParent();
+      }
+    }
+  }
+
+  bool runOnFunction(Function &F) override{
+    do{
+      noLivenessChange = false;
+      findReachableInstructions(F);
+    }while(noLivenessChange);
+
+    eliminateUnreachableInstructions(F);
+
+    return true;
+  }
+};
+}
+
+char OurADCE::ID = 0;
+static RegisterPass<OurADCE> X("our-adce", "OurADCE pass",
+                                          false /* Only looks at CFG */,
+                                          false /* Analysis Pass */);
