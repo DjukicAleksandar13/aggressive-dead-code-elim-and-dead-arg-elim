9730e4b78db383f429ed5b46e9155ee815e6d7bb
DjukicAleksandar13 <djukicaleksandar1305@gmail.com>
2026-08-28 18:40:35 +0200

    Add dead code elimination, slight refactor

--- OurADCE/OurADCEPass.cpp
+++ OurADCE/OurADCEPass.cpp
@@ -3,58 +3,108 @@
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Value.h"
 #include "OurCFG.h"
 
 using namespace llvm;
 
 namespace {
-struct OurADCE : public FunctionPass {
-  std::unordered_map<BasicBlock *, bool> reachableInstructions;
-  bool noLivenessChange;
+	struct OurADCE : public FunctionPass {
+		std::unordered_map<BasicBlock *, bool> reachableInstructions;
+		std::vector<Instruction *> CurrentInstructions;
+		std::vector<Instruction *> InstructionsToRemove;
+		std::unordered_set<Instruction *> LiveInstructions;
+		bool noLivenessChange;
 
-  static char ID;
-  OurADCE() : FunctionPass(ID) {}
+		static char ID;
+		OurADCE() : FunctionPass(ID) {}
 
-  void findReachableInstructions(Function &F){
-    OurCFG *CFG = new OurCFG(F);
-    CFG->DFS(&F.front());
+		void findReachableInstructions(Function &F) {
+			OurCFG *CFG = new OurCFG(F);
+			CFG->DFS(&F.front());
 
-    for(BasicBlock &BB : F){
-      if(CFG->isReachable(&BB) && reachableInstructions[&BB] == false){
-        reachableInstructions[&BB] = true;
-        noLivenessChange = true;
-      }
-    }
-  }
+			for (BasicBlock &BB : F) {
+		  		if (CFG->isReachable(&BB) && reachableInstructions[&BB] == false) {
+		    		reachableInstructions[&BB] = true;
+		  		}
+			}
+		}
 
-  void eliminateUnreachableInstructions(Function &F){
-    std::vector<BasicBlock *> BBList;
-    for(BasicBlock &BB : F){
-      BBList.push_back(&BB);
-    }
+		void eliminateUnreachableInstructions(Function &F) {
+			findReachableInstructions(F);
+			std::vector<BasicBlock *> BBList;
+			for (BasicBlock &BB : F) {
+	  			BBList.push_back(&BB);
+			}
 
-    for(BasicBlock *BB : BBList){
-      if(!reachableInstructions[BB]){
-        BB->eraseFromParent();
-      }
-    }
-  }
+			int countUnreachable = 0;
+			for(BasicBlock *BB : BBList) {
+	  			if (!reachableInstructions[BB]) {
+					countUnreachable++;
+	    			BB->eraseFromParent();
+	  			}
+			}
 
-  bool runOnFunction(Function &F) override{
-    for(BasicBlock &BB : F){
-      reachableInstructions[&BB] = false;
-    }
+			if (countUnreachable == 0) { noLivenessChange = true; }
+		}
 
-    do{
-      noLivenessChange = false;
-      findReachableInstructions(F);
-    }while(noLivenessChange);
+		void addToLive(Instruction *I) {
+			if (LiveInstructions.insert(I).second) {
+				CurrentInstructions.push_back(I);
+			}
+		}
 
-    eliminateUnreachableInstructions(F);
+		void eliminateDeadInstructions(Function &F) {
+			LiveInstructions.clear();
+			CurrentInstructions.clear();
+			InstructionsToRemove.clear();
 
-    return true;
-  }
-};
+			for (BasicBlock &BB : F) {
+				for (Instruction &I : BB) {
+					if (I.isTerminator() || I.mayHaveSideEffects()) {
+						addToLive(&I);
+					}
+				}
+			}
+
+			while (!CurrentInstructions.empty()) {
+				Instruction *currInst = CurrentInstructions.back();
+				CurrentInstructions.pop_back();
+				for (Value *V : currInst->operands()) {
+					if (Instruction *I = dyn_cast<Instruction>(V)) {
+						addToLive(I);
+					}
+				}
+			}
+
+			for (BasicBlock &BB : F) {
+				for (Instruction &I : BB) {
+					if (LiveInstructions.count(&I) != 0) { continue; }
+					InstructionsToRemove.push_back(&I);
+				}
+			}
+
+			if (InstructionsToRemove.size() > 0) { noLivenessChange = true; }
+
+			for (Instruction *Instr : InstructionsToRemove) {
+      			Instr->eraseFromParent();
+    		}
+		}
+
+		bool runOnFunction(Function &F) override {
+			for (BasicBlock &BB : F) {
+  				reachableInstructions[&BB] = false;
+			}
+
+			do {
+  				noLivenessChange = false;
+				eliminateDeadInstructions(F);
+  				eliminateUnreachableInstructions(F);
+			} while (noLivenessChange);
+
+		return true;
+		}
+	};
 }
 
 char OurADCE::ID = 0;
--- OurADCE/OurCFG.cpp
+++ OurADCE/OurCFG.cpp
@@ -1,29 +1,29 @@
 #include "OurCFG.h"
 
-OurCFG::OurCFG(llvm::Function &F){
+OurCFG::OurCFG(llvm::Function &F) {
   FunctionName = F.getName().str();
   CreateCFG(F);
 }
 
-void OurCFG::CreateCFG(Function &F){
-  for(BasicBlock &BB : F){
+void OurCFG::CreateCFG(Function &F) {
+  for(BasicBlock &BB : F) {
     AdjacencyList[&BB] = {};
-    for(BasicBlock *Successor : successors(&BB)){
+    for (BasicBlock *Successor : successors(&BB)) {
         AdjacencyList[&BB].push_back(Successor);
     }
   }
 }
 
-void OurCFG::DFS(llvm::BasicBlock *Current){
+void OurCFG::DFS(llvm::BasicBlock *Current) {
   Visited.insert(Current);
 
   for(BasicBlock *Successor : AdjacencyList[Current]) {
-    if(Visited.find(Successor) == Visited.end()){
+    if (Visited.find(Successor) == Visited.end()) {
       DFS(Successor);
     }
   }
 }
 
-bool OurCFG::isReachable(llvm::BasicBlock *BB){
+bool OurCFG::isReachable(llvm::BasicBlock *BB) {
   return Visited.find(BB) != Visited.end();
 }
